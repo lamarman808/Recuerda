c7b76c4069e6e28bdb13dc9a8eaf4073d9d9f632
Kendal Chaney <lamarman808@gmail.com>
2023-05-05 14:45:43 -0400

    Update-3

---
+++ script.js
@@ -0,0 +1,14 @@
+/*----- constants -----*/
+//const AUDIO = new Audio(
+//REPLACE WITH REMEMBER ME LULLABY DURING GAMEPLAY!)
+//Upon Victory: Chage to MIGUEL DUO VERSION!
+
+/*----- app's state (variables) -----*/
+let victory //When all cards are matched, the player wins!
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+const init = () => {}
