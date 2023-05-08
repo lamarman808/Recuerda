e3dce5ead98540948aa56b9ce254a773dc05ce29
Kendal Chaney <lamarman808@gmail.com>
2023-05-08 10:26:45 -0400

    Images&Cards_Updated

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 //const AUDIO = new Audio(
 //REPLACE WITH REMEMBER ME LULLABY DURING GAMEPLAY!)
 //Upon Victory: Chage to MIGUEL DUO VERSION!
-const cardImg = {
-  front: { img: 'image.png', matches: 'imgs/FILE NAME.png' },
-  back: {
-    img: 'https://www.looper.com/img/gallery/the-big-clue-everyone-missed-early-in-disney-pixars-coco/l-intro-1629645917.jpg',
-    matches: 'imgs/FILE NAME.png'
-  }
+// const cardImg = {
+//   front: { img: 'COCO_PICS/Coco Y Paco.jpeg' },
+//   back: { img: 'COCO_PICS/Skull Mami.jpeg' }
+// }
+let cardEl = document.getElementById('card-1') = {
+  front: { img: 'COCO_PICS/Coco Y Paco.jpeg' },
+  back: { img: 'COCO_PICS/Paco Plays.png' }
 }
 
 /*----- app's state (variables) -----*/
@@ -18,7 +19,7 @@ let back //Matching image/color
 
 /*----- cached element references -----*/
 const messageEl = document.querySelector('h1')
-const cardEl = document.getElementById('card-1')
+
 
 /*----- event listeners -----*/
 document.querySelector('button').addEventListener('click', start)
@@ -26,12 +27,14 @@ cardEl.addEventListener('click', cardFlip)
 
 /*----- functions -----*/
 const start = () => {
-  board = [0]
+  cardEl.style = "background-image: back" 
 }
 
 const renderBoard = () => {} //includes front element
 
-const cardFlip = () => {} //includes front element
+const cardFlip = () => {
+  cardEl = back
+} //includes front element
 
 const checkMatch = () => {} //includes front element
 
