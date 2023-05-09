ca4ef7bcc72e6956a3dc8da0ca97d8c8d83a7428
Kendal Chaney <lamarman808@gmail.com>
2023-05-09 10:35:44 -0400

    3_files_updated

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 /*----- constants -----*/
-//const AUDIO = new Audio(
-//REPLACE WITH REMEMBER ME LULLABY DURING GAMEPLAY!)
-//Upon Victory: Chage to MIGUEL DUO VERSION!
 
 /*----- app's state (variables) -----*/
 let board // Where all the cards appear to be acted upon
@@ -17,25 +14,25 @@ let cardEl = document.getElementById('card-1')
 
 /*----- functions -----*/
 const start = () => {
-  board = [[0, 0, 0, 0][(0, 0, 0, 0)][(0, 0, 0, 0)]]
-  frontImg.style.opacity = 1
-  backImg.style.opacity = 0
+  board = document.getElementById('board')
+  board = [0]
+  victory = null
 
   renderBoard()
 }
 
 const renderBoard = () => {
-  board.forEach(function (colArr, rowArr) {})
+  board.forEach(function () {})
 }
 
 const cardFlip = () => {
   cardEl.classList.toggle('cardBack')
-  //   while ((frontImg = front.evt.target.style.opacity = 1)) {
-  //     backImg = back.style.evt.target.opacity = 0
-  //   }
-  //   while ((frontImg = front.evt.target.style.opacity = 0)) {
-  //     backImg = back.style.evt.target.opacity = 1
-  //   }
+  // while ((frontImg = front.evt.target.style.opacity = 1)) {
+  //   backImg = back.style.evt.target.opacity = 0
+  // }
+  // while ((frontImg = front.evt.target.style.opacity = 0)) {
+  //   backImg = back.style.evt.target.opacity = 1
+  // }
 }
 
 const checkMatch = () => {
