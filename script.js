0cd221770832a694757018b860c831261fd91d53
Kendal Chaney <lamarman808@gmail.com>
2023-05-09 10:32:45 -0400

    verify_update

--- script.js
+++ script.js
@@ -2,43 +2,55 @@
 //const AUDIO = new Audio(
 //REPLACE WITH REMEMBER ME LULLABY DURING GAMEPLAY!)
 //Upon Victory: Chage to MIGUEL DUO VERSION!
-// const cardImg = {
-//   front: { img: 'COCO_PICS/Coco Y Paco.jpeg' },
-//   back: { img: 'COCO_PICS/Skull Mami.jpeg' }
-// }
-let cardEl = document.getElementById('card-1') = {
-  front: { img: 'COCO_PICS/Coco Y Paco.jpeg' },
-  back: { img: 'COCO_PICS/Paco Plays.png' }
-}
 
 /*----- app's state (variables) -----*/
 let board // Where all the cards appear to be acted upon
 let victory //When all cards are matched, the player wins!
-let front //What the player sees initially: Remember THIS!
-let back //Matching image/color
+
+/*-------CARD FACES------------*/
+let frontImg = document.querySelector('front')
+let backImg = document.querySelector('back')
 
 /*----- cached element references -----*/
 const messageEl = document.querySelector('h1')
-
-
-/*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', start)
-cardEl.addEventListener('click', cardFlip)
+let cardEl = document.getElementById('card-1')
 
 /*----- functions -----*/
 const start = () => {
-  cardEl.style = "background-image: back" 
+  board = [[0, 0, 0, 0][(0, 0, 0, 0)][(0, 0, 0, 0)]]
+  frontImg.style.opacity = 1
+  backImg.style.opacity = 0
+
+  renderBoard()
 }
 
-const renderBoard = () => {} //includes front element
+const renderBoard = () => {
+  board.forEach(function (colArr, rowArr) {})
+}
 
 const cardFlip = () => {
-  cardEl = back
-} //includes front element
+  cardEl.classList.toggle('cardBack')
+  //   while ((frontImg = front.evt.target.style.opacity = 1)) {
+  //     backImg = back.style.evt.target.opacity = 0
+  //   }
+  //   while ((frontImg = front.evt.target.style.opacity = 0)) {
+  //     backImg = back.style.evt.target.opacity = 1
+  //   }
+}
 
-const checkMatch = () => {} //includes front element
+const checkMatch = () => {
+  if (cardFlip.target === cardFlip.target) {
+    messageEl.innerHTML = "It's a Match!"
+  } else if (cardFlip.target !== cardFlip.target) {
+    messageEl.innerHTML = 'No bueno. Una vez mas!'
+  }
+}
 
 start() // 1st to finish
 renderBoard() //2nd to finish
 cardFlip() //3rd to finish
 checkMatch() //4th to finish
+
+/*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', start)
+cardEl.addEventListener('click', cardFlip)
