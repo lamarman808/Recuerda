c051d2e363d0b66ac3fc6855fc54ad64074fab11
Kendal Chaney <lamarman808@gmail.com>
2023-05-11 11:01:48 -0400

    all_cards_visible

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 /*----- constants -----*/
+const cocoPics = {
+  c1: { img: 'COCO_PICS/cocoypaco.png', matches: 'c2' },
+  c2: { img: 'COCO_PICS/headlesshector.png', matches: 'c2' },
+  c3: { img: 'COCO_PICS/pacoplays.png', matches: 'c3' },
+  c4: { img: 'COCO_PICS/pacopose.png', matches: 'c4' }
+  // c5: { img: 'imgs/skullmami.png', matches: 'c5' },
+}
+
+console.log(cocoPics)
 
 /*----- app's state (variables) -----*/
 let board // Where all the cards appear to be acted upon
 let victory //When all cards are matched, the player wins!
 
 /*-------CARD FACES------------*/
-let frontImg = document.querySelector('front')
+// let frontImg = document.querySelector('front')
 let backImg = document.querySelector('back')
 
 /*----- cached element references -----*/
@@ -25,14 +34,23 @@ const start = () => {
 // }
 
 const cardFlip = (idx) => {
-  console.log('CARD FLIP!')
   // cardEl.style.transform = rotateY(180deg)
   // cardEl.classList.remove('front')
-  cardEl[idx].classList.add('cardBack')
+  cardEl[idx].classList.toggle('cardBack')
   checkMatch()
 }
 
+start() // 1st to finish
+// renderBoard() //2nd to finish
+
+/*-----------VICTORY LOGIC!-------------*/
 const checkMatch = () => {
+  //   if (cardFlip.target === cardFlip.target) {
+  //     messageEl.innerText = "It's a Match!"
+  //   } else if (cardFlip.target !== cardFlip.target) {
+  //     messageEl.innerText = 'No bueno. Una vez mas!'
+  //   }
+  // }
   if (cardFlip.target === cardFlip.target) {
     messageEl.innerText = "It's a Match!"
   } else if (cardFlip.target !== cardFlip.target) {
@@ -40,25 +58,14 @@ const checkMatch = () => {
   }
 }
 
-start() // 1st to finish
-// renderBoard() //2nd to finish
-// cardFlip() //3rd to finish
-// checkMatch() //4th to finish
-
-/*-----------VICTORY LOGIC!-------------*/
-console.log(cardEl)
 /*----- event listeners -----*/
+// For Loop Required to iterate through the Array of CARD ELEMENTS to select which in the Index receives the CLICK event!
 cardEl.forEach((el, index) => {
   el.addEventListener('click', () => {
     cardFlip(index)
   })
 })
 document.querySelector('button').addEventListener('click', start)
-
-// cardOne.addEventListener('click', cardFlip)
-// cardTwo.addEventListener('click', cardFlip)
-// cardThree.addEventListener('click', cardFlip)
-// cardFour.addEventListener('click', cardFlip)
 
 /*-----------WORKS CITED------------*/
 //
